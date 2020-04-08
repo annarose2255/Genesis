90b4d4dd0a89b4b32c06cca9efcf23ec4b4dd897
Andrew Lewis <anjale98@gmail.com>
2020-04-08 05:27:20 -0400

    Made it easier to run different demos

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,6 +5,7 @@
 #include "MyGame.h"
 #include "DevTool.h"
 #include "SheetDemo.h"
+#include "QuestDemo.h"
 #include "CollisionDemo.h"
 #include "AnimatedSprite.h"
 #include "rapidxml_utils.hpp"
@@ -12,48 +13,73 @@
 using namespace std;
 using namespace rapidxml;
 
+static void show_usage(std::string name)
+{
+	std::cerr << "Usage: " << name << " <option(s)> SOURCES\n"
+			<< "Options:\n"
+			<< "\t<no flags>\t\tStarts Game\n"
+			<< "\t-h,--help\t\tShow this help message\n"
+			<< "\t-d\t\t\tStarts Devtool\n"
+			<< "\t-c\t\t\tStarts Collision Demo\n"
+			<< "\t-q\t\t\tStarts Quest Demo\n"
+			<< "\t-s\t\t\tStarts Sheet Demo\n"
+			<< std::endl;
+}
+
 int main(int argc, char ** argv)
 {
-  
-//     SheetDemo* sheetDemo = new SheetDemo();
-//     sheetDemo->start();
-//     delete sheetDemo;
-//     return 0;
 
-    // CollisionDemo* collisionDemo = new CollisionDemo();
-    // collisionDemo->start();
-    // delete collisionDemo;
-    // return 0;
+	// // /*
+	//     // Testing rapid xml
+	//     file<> xmlFile("./resources/quest/Gold.xml");
+	//     xml_document<> doc;
+	//     doc.parse<0>(xmlFile.data());
+	//     cout << doc.first_node()->name() << endl;
+	//     xml_node<> *node = doc.first_node("TextureAtlas");
+	//     cout << node->first_node()->first_attribute()->next_attribute("w")->value() << endl;
+	// */
 
-// // /*
-//     // Testing rapid xml
-//     file<> xmlFile("./resources/quest/Gold.xml");
-//     xml_document<> doc;
-//     doc.parse<0>(xmlFile.data());
-//     cout << doc.first_node()->name() << endl;
-//     xml_node<> *node = doc.first_node("TextureAtlas");
-//     cout << node->first_node()->first_attribute()->next_attribute("w")->value() << endl;
-// */
-
-/*
-    AnimatedSprite* anim = new AnimatedSprite("Gold", "./resources/quest/Gold.png", "./resources/quest/Gold.xml");
-*/
-	DevTool* devTool = new DevTool();
-    devTool->start();
-
-	delete devTool;
+	// takes one command line argument
+	if (argc == 2){
+		if ( string(argv[1]) == "-h" || string(argv[1]) == "--help" ){
+			show_usage(argv[0]);
+		}
+		else if ( string(argv[1]) == "-c" ){
+			cout << "Starting Collision Demo..." << endl;
+			CollisionDemo* collisionDemo = new CollisionDemo();
+			collisionDemo->start();
+			delete collisionDemo;
+		}
+		else if (string(argv[1]) == "-d"){
+			cout << "Starting DevTool..." << endl;
+			DevTool* devTool = new DevTool();
+			devTool->start();
+			delete devTool;
+		}
+		else if (string(argv[1]) == "-q"){
+			cout << "Starting Sheet Demo..." << endl;
+			SheetDemo* sheetDemo = new SheetDemo();
+			sheetDemo->start();
+			delete sheetDemo;
+		} 
+		else if (string(argv[1]) == "-s"){
+			cout << "Starting Sheet Demo..." << endl;
+			SheetDemo* sheetDemo = new SheetDemo();
+			sheetDemo->start();
+			delete sheetDemo;
+		} 
+		else {
+			std::cerr << "Invalid flag: " << argv[1] << "\n";
+			show_usage(argv[0]);
+		}
+	} else if (argc == 1) {
+		cout << "Starting MyGame..." << endl;
+		MyGame* myGame = new MyGame();
+		myGame->start();
+		delete myGame;
+	} else {
+		std::cerr << "Invalid arguments: This program only takes one argument\n";
+		show_usage(argv[0]);
+	}
 	return 0;
-
-
-	// MyGame* myGame = new MyGame();
- //    myGame->start();
-
-	// delete myGame;
-	// return 0;
-
-	// QuestDemo* questDemo = new QuestDemo();
-	// questDemo->start();
-
-	// delete questDemo;
-	// return 0;
 }
