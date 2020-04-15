1f68c77f0f6ea3e7b8c9c869af4cfc1839919f65
anna <ak9ct@virginia.edu>
2020-04-15 18:17:56 -0400

    added basic character files and resolved issues

---
+++ bin/src/main/events/FightEvent.cpp.d
@@ -0,0 +1,36 @@
+bin/./src/main/events/FightEvent.cpp.o: src/main/events/FightEvent.cpp \
+ src/main/events/FightEvent.h src/engine/events/Event.h \
+ src/engine/events/EventDispatcher.h src/engine/events/Event.h \
+ src/engine/events/EventListener.h src/engine/DisplayObjectContainer.h \
+ src/engine/DisplayObject.h src/engine/AffineTransform.h \
+ src/engine/Scene.h src/engine/DisplayObjectContainer.h \
+ src/engine/Sprite.h src/engine/Layer.h src/engine/AnimatedSprite.h \
+ src/engine/json.hpp
+
+src/main/events/FightEvent.h:
+
+src/engine/events/Event.h:
+
+src/engine/events/EventDispatcher.h:
+
+src/engine/events/Event.h:
+
+src/engine/events/EventListener.h:
+
+src/engine/DisplayObjectContainer.h:
+
+src/engine/DisplayObject.h:
+
+src/engine/AffineTransform.h:
+
+src/engine/Scene.h:
+
+src/engine/DisplayObjectContainer.h:
+
+src/engine/Sprite.h:
+
+src/engine/Layer.h:
+
+src/engine/AnimatedSprite.h:
+
+src/engine/json.hpp:
---
+++ bin/src/main/events/SceneManager.cpp.d
@@ -0,0 +1,46 @@
+bin/./src/main/events/SceneManager.cpp.o: \
+ src/main/events/SceneManager.cpp src/main/events/SceneManager.h \
+ src/engine/events/Event.h src/engine/events/EventDispatcher.h \
+ src/engine/events/Event.h src/engine/events/EventListener.h \
+ src/engine/events/EventListener.h src/engine/AnimatedSprite.h \
+ src/engine/Sprite.h src/engine/DisplayObjectContainer.h \
+ src/engine/DisplayObject.h src/engine/AffineTransform.h \
+ src/engine/Scene.h src/engine/Layer.h src/engine/AnimatedSprite.h \
+ src/engine/json.hpp src/main/events/FightEvent.h \
+ src/engine/DisplayObjectContainer.h src/main/events/ChangeSceneEvent.h
+
+src/main/events/SceneManager.h:
+
+src/engine/events/Event.h:
+
+src/engine/events/EventDispatcher.h:
+
+src/engine/events/Event.h:
+
+src/engine/events/EventListener.h:
+
+src/engine/events/EventListener.h:
+
+src/engine/AnimatedSprite.h:
+
+src/engine/Sprite.h:
+
+src/engine/DisplayObjectContainer.h:
+
+src/engine/DisplayObject.h:
+
+src/engine/AffineTransform.h:
+
+src/engine/Scene.h:
+
+src/engine/Layer.h:
+
+src/engine/AnimatedSprite.h:
+
+src/engine/json.hpp:
+
+src/main/events/FightEvent.h:
+
+src/engine/DisplayObjectContainer.h:
+
+src/main/events/ChangeSceneEvent.h:
