cf57dd7e82c3a37049e6afbda8073761030a8072
anna <daytonkaroll@outlook.com>
2020-05-01 14:32:30 -0400

    trying to get rooms to load - not working currently

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -32,9 +32,9 @@ MyGame::MyGame() : Game(800, 700) { //rendered space
 	eDispatcher->addEventListener(collisionSystem, DO_ADDED_EVENT);
     scene1 = new Scene();
     // scene1->loadScene("./resources/scenes/character.json");
-	scene1->loadTileMap("./resources/scenes/area1files/Area1Room7.json");
-	scene1->loadScene("./resources/scenes/ghostchar.json");
-	scene1->loadScene("./resources/scenes/Room7.json"); //contains objects and enemies
+	scene1->loadTileMap("./resources/scenes/area1files/Area1Room1.json");
+	//scene1->loadScene("./resources/scenes/ghostchar.json");
+	//scene1->loadScene("./resources/scenes/Room1.json"); //contains objects and enemies
 	//test playable char
 	// AnimatedSprite* chara = new AnimatedSprite("chara"); 
 	// chara->addSSAnimation("./resources/ghostchar/idle.png", "./resources/ghostchar/idle.xml");
