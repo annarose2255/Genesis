eb33cfb77840fcb83ebcde3bd9cbbce837257247
anna <daytonkaroll@outlook.com>
2020-05-01 23:56:06 -0400

    fix

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -46,9 +46,9 @@ MyGame::MyGame() : Game(800, 700) { //rendered space
 	bg->addChild(caveIMG);
 	caveIMG->visible = false;
 	// forestBG->getChild(0)->visible = false;
-	scene1->loadTileMap("./resources/scenes/area1files/Area1Room6.json");
+	scene1->loadTileMap("./resources/scenes/area1files/Area1Room1.json");
 	scene1->loadScene("./resources/scenes/ghostchar.json");
-	scene1->loadScene("./resources/scenes/Room6.json"); //contains objects and enemies
+	scene1->loadScene("./resources/scenes/Room1.json"); //contains objects and enemies
 	//test playable char
 	// AnimatedSprite* chara = new AnimatedSprite("chara"); 
 	// chara->addSSAnimation("./resources/ghostchar/idle.png", "./resources/ghostchar/idle.xml");
@@ -364,10 +364,10 @@ void MyGame::update(set<SDL_Scancode> pressedKeys, set<SDL_GameControllerButton>
     Game::camera->camera.x =  currentScene->position.x + currentScene->width/2 - 400;
 	Game::camera->camera.y =  currentScene->position.y + currentScene->height/2 - 350;
 
-	cout << "Camera x " << Game::camera->position.x << endl; 
+	/* cout << "Camera x " << Game::camera->position.x << endl; 
 	cout << "Camera y " << Game::camera->position.y << endl; 
 	cout << "Character x " << currentScene->getPlayer()->position.x << endl;
-	cout << "Character y " << currentScene->getPlayer()->position.y << endl; 
+	cout << "Character y " << currentScene->getPlayer()->position.y << endl;  */
 	if( Game::camera->camera.x < 0){
 		Game::camera->camera.x = 0;
 	}
